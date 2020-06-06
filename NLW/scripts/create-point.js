085c43fef9a5c4315f6ebe88d205e8650da2154e
marcio-eduardo <marcioeduardo.ti@gmail.com>
2020-06-06 01:45:52 -0300

    Aula 2 - 06/06/2020 - 02hs

---
+++ NLW/scripts/create-point.js
@@ -0,0 +1,49 @@
+//() => {} (Arrow function. Função anonima)
+
+//Pesquisa todos os estados do país e armazena na variável states, depois localiza o estado selecionado e insere no campo state
+function populateUFs() {
+    const ufSelect = document.querySelector("select[name=uf]")
+    
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    .then( ( res ) => { return res.json() }) //.then( res =>  res.json() ) Pode ser usado dessa forma neste caso
+    .then( states => {
+        
+        for ( const state of states ) {
+            ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`           
+        }
+    } )
+}
+
+//chama a function de mesmo nome
+populateUFs()
+
+// Verifica o estado selecionado e seleciona as suas respectivas cidades. Por fim permite selecionar a cidade no campo city 
+function getCities(event){
+    const citySelect = document.querySelector("[name=city]")
+    const stateInput = document.querySelector("[name=state")
+    
+    const ufValue = event.target.value
+
+    const indexOfSelectState = event.target.selectedIndex
+    stateInput.value = event.target.options[indexOfSelectState].text
+
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+
+    fetch(url)
+    .then( ( res ) => { return res.json() }) //.then( res =>  res.json() ) Pode ser usado dessa forma neste caso
+    .then( cities => {
+
+        for ( const city of cities ) {
+            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`           
+        }
+
+        citySelect.disabled = false
+    } )
+}
+
+
+document
+    .querySelector("select[name=uf]")
+    .addEventListener("change",  getCities)
+  
+
---
+++ NLW/scripts/intro.js
@@ -0,0 +1,76 @@
+//Coments
+//document.write("Hello World!")
+
+// variáveis, tipos de dados
+//let myvar 
+//myvar = "Hello World!"
+//document.write(myvar)
+//const myconst = "He"
+
+//document.write(myconst + myvar)
+
+//String
+
+//"Isso é uma string"
+//'Isoo também é uma string'
+//`Isso é uma string também`
+
+
+//number
+//const n1 = 1
+//const n2 = 12
+//document.write(n1 + n2)
+
+// boolean - true ou false
+//const bTrue = true
+//const bFalse = false
+
+//Objeto
+//Possuem propriedades  e funcionalidades
+//Possuem atributops e métodos
+/*const pessoa = {
+  altura: "1,80m",
+  idade: 24, 
+  solteiro: true,
+  correr(){
+    return "Run Forest"    
+  }
+}
+
+document.write(pessoa.correr())
+*/
+
+/*Array - Vetores
+const colecaoDeBolinhas = ["Blue", "Green", 1, {name:"My Name"}]
+
+document.write(colecaoDeBolinhas[3].name)
+*/
+
+/*Funções - Funcionalidades - Atalhos - Reuso de Código
+// registrar função
+// recebe na variavel name 
+function sayMyName(name) {
+  document.write(name)
+}
+//Executar
+sayMyName("Marcio")
+*/
+
+
+/* condicionais
+
+const notaFinal = 7
+
+if (notaFinal < 5) {
+  document.write("Aluno reprovado!")
+} else {
+  document.write("Aluno aprovado!")
+}
+*/
+
+// loop Repetições
+
+//for (i = 0; i < 10; i++){
+//  document.write(`<p>${i}</p>`)
+//}
+
